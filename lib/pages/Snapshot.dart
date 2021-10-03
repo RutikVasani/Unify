531a255787a5913110f6c6d929514fb735e7f57b
codesachin501 <codesachin501@gmail.com>
2021-10-03 14:06:41 +0530

    l

--- lib/pages/Snapshot.dart
+++ lib/pages/Snapshot.dart
@@ -1,7 +1,7 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_application_1/pages/login_page.dart';
-// import 'package:flutter_application_1/pages/main_page.dart';
+import 'package:flutter_application_1/pages/main_page.dart';
 // import 'package:flutter_application_1/utils/routes.dart';
 
 class Snapshot extends StatelessWidget {
@@ -13,7 +13,7 @@ class Snapshot extends StatelessWidget {
             if (snapshot.connectionState == ConnectionState.waiting) {
               return Center(child: CircularProgressIndicator());
             } else if (snapshot.hasData) {
-              return Center(child: Text('Some error detected'));
+              return MainPage();
             } else if (snapshot.hasError) {
               return Center(child: Text('Some error detected'));
             } else {
